c8b17f323043c6f1cc3cb0259b9780af3bce34b7
malko <jgotti@jgotti.org>
2014-08-27 03:06:53 +0200

    add json sync methods to fspromised

--- libs/fs-promised.js
+++ libs/fs-promised.js
@@ -9,12 +9,33 @@ methods.forEach(function(m){
 	fs[m + 'Promise'] = D.nodeCapsule(fs, fs[m]);
 });
 
+fs.readJsonSync = function readJsonSync(filename, options, callback){
+	return fs.readFileSync(filename, options, function(err, data){
+		if( !err ){
+			try{
+				data = JSON.parse(data);
+			}catch(e){
+				return callback(e);
+			}
+			callback(err,data);
+		}
+		callback(err, data);
+	});
+};
 fs.readJsonPromise = function readJsonPromise(filename, options){
 	return fs.readFilePromise(filename, options).success(JSON.parse);
-}
-
+};
+fs.writeJsonSync = function writeJsonSync(filename, data, options, callback){
+	try{
+		data = JSON.stringify(data);
+	}catch(e){
+		callback(e);
+		return;
+	}
+	return fs.writeFileSync(filename, data, options, callback);
+};
 fs.writeJsonPromise = function writeJsonPromise(filename, data, options){
 	return fs.writeFilePromise(filename, JSON.stringify(data), options);
-}
+};
 
 module.exports = fs;
