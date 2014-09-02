90268f19150ee7d88229c0369e3a6244d0df03a5
malko <jgotti@jgotti.org>
2014-09-02 03:32:54 +0200

    use test-runner as a middleware

--- bin/runner.js
+++ bin/runner.js
@@ -8,13 +8,13 @@ var connect = require('connect')
 	, execPromise = D.nodeCapsule(childProcess, childProcess.exec)
 	, server = http.createServer(app)
 	, socketio = require('socket.io')(server)
-	, cleanExp = /[^a-z_0-9-]/ig
+	// , cleanExp = /[^a-z_0-9-]/ig
 	, nightwatchConfig = null
 	, settings = require('../libs/opt-parsing.js')
 	, testPreparator = require('../libs/test-prepare.js')
 	, testList = require('../libs/test-list.js')
-	, testStatesCache = testList.cache
-	, runningTestsPromise = {}
+	, testRunner = require('../libs/test-runner.js')(settings, testList, testPreparator)
+	// , runningTestsPromise = {}
 	, updatePromise = null
 ;
 
@@ -28,9 +28,9 @@ function noCache(res){
 	res.setHeader('Pragma', 'no-cache');
 	return res;
 }
-function cleanName(name){
-	return name.replace(cleanExp,'');
-}
+// function cleanName(name){
+// 	return name.replace(cleanExp,'');
+// }
 function setEnvs(){
 	// tell connected clients about the new env
 	//socketio.emit('setEnvs', nightwatchConfig.getEnvs());
@@ -55,6 +55,7 @@ setEnvs();
 
 // load test data
 testList.setCachePath('.whitewalker/').load();
+testList.registerObserver(socketio);
 
 // return the index file with list of available tests
 app
@@ -63,14 +64,14 @@ app
 	.on('/tests/?', function(req, res, next){
 		noCache(res);
 		res.setHeader('Content-type', 'application/json; charset=utf8');
-		res.end(JSON.stringify(testStatesCache));
+		res.end(JSON.stringify(testList.cache));
 		next();
 	})
 	/*.on('/tests/([a-zA-Z0-9_-]+])/', function(req, res, next, testName){
 		console.log('TEST', testName);
 		noCache(res);
-		if (testName in testStatesCache) {
-			res.end(JSON.stringify(testStatesCache[testName]));
+		if (testName in testList.cache) {
+			res.end(JSON.stringify(testList.cache[testName]));
 		} else {
 			res.writeHead(404);
 			res.end();
@@ -90,81 +91,12 @@ app
 		res.end();
 		next();
 	})
-	.on('/run/([a-zA-Z0-9_-]+)(?:/([a-zA-Z0-9_-]+))?', function(req, res, next, testName, environment){
-		testName = cleanName(testName);
-		environment = environment ? cleanName(environment) : 'all';
-		var test = testList.initEnvTest(environment, true)
-			, testSucceed = function(result){
-				test.status = 'ok';
-				test.out = result;
-				res.end(JSON.stringify(test));
-			}
-			, testFailed = function(err){
-				test.status = 'failed';
-				test.out = err.toString();
-			}
-			, end = function(){
-				test.endTime = new Date();
-				test.duration = (test.endTime.getTime() - test.startTime.getTime())/1000 + 's';
-				testList.setTestResult(testName, environment, test);
-				delete runningTestsPromise[testName + '/' + environment];
-				res.end(JSON.stringify({name:testName, environment:environment, result:test}));
-				socketio.emit('setTest', testName, environment, test);
-				next();
-			}
-		;
-		if( (testName + '/' + environment) in runningTestsPromise){
-			return runningTestsPromise[testName + '/' + environment]
-				.success(testSucceed)
-				.error(testFailed)
-				.ensure(end)
-				.rethrow()
-			;
-		}
-		console.log('running',  testName + '/' + environment)
-		testStatesCache[testName].tests[environment] = test;
-		socketio.emit('setTest', testName, environment, test);
-		runningTestsPromise[testName + '/' + environment] = testPreparator.prepare(testName, settings.paths.tests, settings.paths.tmp, environment)
-			.success(function(testFilename){
-				var d = D();
-				childProcess.exec(
-					settings.paths.nightwatch + ' -e ' + environment + ' -c ./nightwatch.json -t ' + testFilename
-					, function(err, stdout, stderr){
-						if( err ){
-							d.reject( ((stderr ? stderr : stdout) || err ).toString('utf8').replace(/\[[0-9;]+m/g,''));
-							return;
-						}
-						d.resolve(stdout.toString('utf8').replace(/\[[0-9;]+m/g,''));
-					}
-				);
-				return d.promise;
-			})
-			.success(testSucceed)
-			.error(testFailed)
-			.ensure(end)
-			.rethrow()
-		;
-	})
+	.on('/run/([a-zA-Z0-9_-]+)(?:/([a-zA-Z0-9_-]+))?', testRunner)
 	.use(connect.static(__dirname + '/../public/'))
 	.use(connect.static(__dirname + '/../templates/'))
 	.use(connect.static(__dirname + '/../node_modules/d.js/lib/'))
 ;
-/*if(settings.livereload){
-	console.log('reloade', __dirname + '/../public',require('stylus').middleware({
-		src: __dirname + '/../public',
-		dest: __dirname + '/../public'
-	}))
-	app.use(require('stylus').middleware({
-		src: __dirname + '/../public',
-		dest: __dirname + '/../public'
-	}));
-}
-/*socketio.on('connection', function(socket){
-	console.log('a user connected to socketio');
-	//socket.emit('setEnvs', nightwatchConfig.getEnvs());
-	socket.emit('setTests', testStatesCache);
-
-});*/
+//-- stylus live reload in dev mode
 if( settings.livereload ){
 	var path = require('path')
 		, stylFile = path.normalize(__dirname + '/../public/styles.styl')
@@ -182,12 +114,13 @@ if( settings.livereload ){
 				}
 				fs.writeFilePromise(cssFile, css, 'utf8').success(function(){
 					socketio.emit('livereload');
-				})
+				});
 			});
 		});
 	});
 }
 
+//-- starting the server
 server.listen(settings.port, function(){
 	console.log("Walking at http://127.0.0.1:%s", settings.port);
 
